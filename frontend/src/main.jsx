e640f18b9ea92b583d20659dec4473698c9e9ed7
kamleshgoswami09 <knightlykamlesh09@gmail.com>
2026-02-07 11:32:47 +0530

    Refactor main.jsx to use Vite configuration

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,24 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { defineConfig as cfg } from "vite";
+import reactPlugin from "@vitejs/plugin-react";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const createApiProxy = (endpoint) => ({
+  "/api": Object.assign(
+    {},
+    {
+      target: endpoint,
+      changeOrigin: true
+    }
+  )
+});
+
+const createDevServer = () => {
+  const backend = "http://127.0.0.1:8000";
+  return {
+    proxy: createApiProxy(backend)
+  };
+};
+
+export default cfg(() => ({
+  plugins: [reactPlugin()],
+  server: createDevServer()
+}));
